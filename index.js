0676145e938435e7eb9c08709c7e6812c757b029
altafziyaa <altafziyaa91@gmail.com>
2026-02-07 19:12:09 +0530

    debugging

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-import express, { json } from "express";
+import express from "express";
 import cartRouter from "./src/router/cart.router.js";
 import connectDB from "./src/config/db.js";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const app = express();
-app.use(json());
-const PORT = process.env.PORT || 4004;
+app.use(express.json());
 
-app.use("api/v1", cartRouter);
+app.use("/api/v1", cartRouter);
 connectDB();
-app.listen(PORT, () => {
-  console.log(`app running on http://localhost:${PORT}`);
-});
-
-// http://localhost:3000/api/v1/addcart
+export default app;
--- src/router/cart.router.js
+++ src/router/cart.router.js
@@ -1,14 +1,12 @@
 import express from "express";
 import cartController from "../controller/cart.controller.js";
+import { authMiddleware } from "../middleware/cart.middleware.js";
 
 const router = express.Router();
 
-router.post("/addcart", cartController.addToCart);
-router.patch("/quantity", cartController.updateCartQuantity);
-router.delete("/delete", cartController.removeFromCart);
-router.delete("/clear", cartController.clearCart);
+router.post("/addcart", authMiddleware, cartController.addToCart);
+router.patch("/quantity", authMiddleware, cartController.updateCartQuantity);
+router.delete("/delete", authMiddleware, cartController.removeFromCart);
+router.delete("/clear", authMiddleware, cartController.clearCart);
 
 export default router;
-
-// 6974e7cfb687005773ebb0ab -productId
-// 696b88799f2d541b6fb983b4 - userId
